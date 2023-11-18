49659538532dad7b17c78fe7615e08c1603270c8
Iurii Kurinnoi <yur4kur@icloud.com>
2023-11-18 10:57:25 +0500

    Initial Commit

---
+++ Lesson-4.01_TrafficLight/Lesson_4_01_TrafficLightApp.swift
@@ -0,0 +1,17 @@
+//
+//  Lesson_4_01_TrafficLightApp.swift
+//  Lesson-4.01_TrafficLight
+//
+//  Created by Юрий Куринной on 18.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct Lesson_4_01_TrafficLightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
